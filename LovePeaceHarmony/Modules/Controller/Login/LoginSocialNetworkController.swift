b46ccfca1ecf6dde310f396fe0faa4176081c275
Cass Pangell <casspangell@Casss-MacBook-Pro.local>
2025-05-25 21:50:30 -0600

    Reset password ui

--- LovePeaceHarmony/Modules/Controller/Login/LoginSocialNetworkController.swift
+++ LovePeaceHarmony/Modules/Controller/Login/LoginSocialNetworkController.swift
@@ -319,7 +319,6 @@ class LoginSocialNetworkController: BaseViewController, IndicatorInfoProvider, U
                     updatedConfig?.baseForegroundColor = .white
                     loginButton.configuration = updatedConfig
                     
-                    // Display error in the error label
                     let errorMessage: String
                     
                     if let errorCode = AuthErrorCode.errorCode(from: error) {
@@ -448,6 +447,8 @@ class LoginSocialNetworkController: BaseViewController, IndicatorInfoProvider, U
     }
     
     private func isValidEmail(_ email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
         return emailPredicate.evaluate(with: email)
     }
     
