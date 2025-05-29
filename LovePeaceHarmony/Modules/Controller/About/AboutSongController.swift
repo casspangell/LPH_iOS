7a45f7b93e8b70f2903e03554f5b610be5c6de58
Cass Pangell <casspangell@Casss-MacBook-Pro.local>
2025-05-29 11:35:16 -0600

    Added mp4 video

--- LovePeaceHarmony/Modules/Controller/About/AboutSongController.swift
+++ LovePeaceHarmony/Modules/Controller/About/AboutSongController.swift
@@ -9,13 +9,15 @@
 
 import UIKit
 import XLPagerTabStrip
-import youtube_ios_player_helper
+import AVFoundation
+import AVKit
 
-class AboutSongController: BaseViewController, IndicatorInfoProvider, YTPlayerViewDelegate {
+class AboutSongController: BaseViewController, IndicatorInfoProvider {
 
     // MARK: Variables
     
     var constraintCalculated = false
+    private var videoCoverImageView: UIImageView?
     
     // MARK: - IBOutlets
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
@@ -33,15 +35,34 @@ class AboutSongController: BaseViewController, IndicatorInfoProvider, YTPlayerVi
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
-        if !constraintCalculated {
-            let youtubePlayer: YTPlayerView = YTPlayerView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: viewVideoContainer.frame.height))
-            activityIndicator.startAnimating()
-            youtubePlayer.delegate = self
-            youtubePlayer.load(withVideoId: YOUTUBE_SONG_ID)
-            viewVideoContainer.addSubview(youtubePlayer)
-            constraintCalculated = true
+        // Remove any previous subviews (e.g., old YTPlayerView or cover)
+        viewVideoContainer.subviews.forEach { $0.removeFromSuperview() }
+        activityIndicator.stopAnimating()
+        viewLoadingContainer.isHidden = true
+
+        // Generate and show the first frame as cover
+        if let coverImage = getFirstFrameOfVideo() {
+            let coverImageView = UIImageView(image: coverImage)
+            coverImageView.contentMode = .scaleAspectFill
+            coverImageView.clipsToBounds = true
+            coverImageView.frame = viewVideoContainer.bounds
+            coverImageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            viewVideoContainer.addSubview(coverImageView)
+            videoCoverImageView = coverImageView
         }
-        
+
+        // Add a play button overlay
+        let playButton = UIButton(type: .custom)
+        playButton.setImage(UIImage(systemName: "play.circle.fill"), for: .normal)
+        playButton.tintColor = .white
+        playButton.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        playButton.layer.cornerRadius = 32
+        playButton.clipsToBounds = true
+        playButton.frame = CGRect(x: (viewVideoContainer.bounds.width - 64) / 2, y: (viewVideoContainer.bounds.height - 64) / 2, width: 64, height: 64)
+        playButton.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin, .flexibleTopMargin, .flexibleBottomMargin]
+        playButton.addTarget(self, action: #selector(playLocalVideo), for: .touchUpInside)
+        viewVideoContainer.addSubview(playButton)
+
         let songTitle = NSMutableAttributedString(string: NSLocalizedString(AboutDescription.songTitle, comment: "") )
         labelTitle.attributedText = songTitle
         
@@ -56,20 +77,45 @@ class AboutSongController: BaseViewController, IndicatorInfoProvider, YTPlayerVi
         descriptionFour.text = songDescription4
     }
 
+    @objc func playLocalVideo() {
+        guard let videoURL = Bundle.main.url(forResource: "how_to_change_the_world", withExtension: "mp4") else {
+            print("Error: Could not find how_to_change_the_world.mp4")
+            showVideoError()
+            return
+        }
+        
+        let player = AVPlayer(url: videoURL)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        present(playerViewController, animated: true) {
+            player.play()
+        }
+    }
+
+    func showVideoError() {
+        let alert = UIAlertController(title: "Video Error", message: "Unable to load video", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+
+    private func getFirstFrameOfVideo() -> UIImage? {
+        guard let videoURL = Bundle.main.url(forResource: "how_to_change_the_world", withExtension: "mp4") else {
+            return nil
+        }
+        let asset = AVAsset(url: videoURL)
+        let imageGenerator = AVAssetImageGenerator(asset: asset)
+        imageGenerator.appliesPreferredTrackTransform = true
+        do {
+            let cgImage = try imageGenerator.copyCGImage(at: kCMTimeZero, actualTime: nil)
+            return UIImage(cgImage: cgImage)
+        } catch {
+            print("Error generating video cover image: \(error)")
+            return nil
+        }
+    }
+
     // MARK: - XLPagerTabStrip
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
         return IndicatorInfo(title: "Title")
     }
-    
-    // MARK: - YTPlayerViewDelegate
-    func playerViewDidBecomeReady(_ playerView: YTPlayerView) {
-        viewLoadingContainer.removeFromSuperview()
-    }
-    
-    func playerView(_ playerView: YTPlayerView, didChangeTo state: YTPlayerState) {
-        if state == .playing {
-            AVAudioManager.sharedInstance.pause()
-        }
-    }
-
 }
